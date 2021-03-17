86983907fd72d97bb9b142ec925cc08886d31cd9
Vagner Wentz <wentz.vagner@gmail.com>
2021-03-16 21:26:37 -0300

    feat: separate the navbar to SideBar component and the movies to a Content Movie component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import { useEffect, useState } from 'react';
 
-import { Button } from './components/Button';
-import { MovieCard } from './components/MovieCard';
-
-// import { SideBar } from './components/SideBar';
-// import { Content } from './components/Content';
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
 
 import { api } from './services/api';
 
 import './styles/global.scss';
-
-import './styles/sidebar.scss';
-import './styles/content.scss';
-
 interface GenreResponseProps {
   id: number;
   name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
   title: string;
 }
-
 interface MovieProps {
   Title: string;
   Poster: string;
@@ -32,16 +24,8 @@ interface MovieProps {
 export function App() {
   const [selectedGenreId, setSelectedGenreId] = useState(1);
 
-  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
-
   const [movies, setMovies] = useState<MovieProps[]>([]);
   const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
-
-  useEffect(() => {
-    api.get<GenreResponseProps[]>('genres').then(response => {
-      setGenres(response.data);
-    });
-  }, []);
 
   useEffect(() => {
     api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
@@ -53,28 +37,9 @@ export function App() {
     })
   }, [selectedGenreId]);
 
-  function handleClickButton(id: number) {
-    setSelectedGenreId(id);
-  }
-
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <nav className="sidebar">
-        <span>Watch<p>Me</p></span>
-
-        <div className="buttons-container">
-          {genres.map(genre => (
-            <Button
-              id={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
-        </div>
-
-      </nav>
+      <SideBar selectedGenreId={selectedGenreId} setSelectedGenreId={setSelectedGenreId}/>
 
       <div className="container">
         <header>
@@ -82,11 +47,7 @@ export function App() {
         </header>
 
         <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
+          <Content movies={movies} />
         </main>
       </div>
     </div>
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,24 @@
-export function Content() {
-  // Complete aqui
+import '../styles/content.scss';
+
+import { MovieCard } from './MovieCard';
+interface ContentProps {
+  movies: Array<{
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }>
+}
+
+export function Content({ movies }: ContentProps) {
+  return (
+    <div className="movies-list">
+      {movies.map(movie => (
+        <MovieCard key={movie.Title} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+      ))}
+    </div>
+  )  
 }
